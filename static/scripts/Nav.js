0f186ade180a6c070d27e17b2466bcbef27c6696
ramisaferdous <rifahramisa@iut-dhaka.edu>
2024-04-13 22:26:03 +0600

    Rahanuma Ryaan made changes

---
+++ static/scripts/Nav.js
@@ -0,0 +1,16 @@
+function Nav() {
+    return <nav>
+      <div class="container">
+          <h1 >Knock</h1>
+          <ul>
+              <li><a href="/home">Home</a></li>
+              <li><a href="/projects">About</a></li>
+              <li><a href="#">Services</a></li>
+              <li><a href="#">Contact</a></li>
+          </ul>
+      </div>
+  </nav>
+    
+  }
+  
+  export default Nav;
